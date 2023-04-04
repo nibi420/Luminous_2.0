b0a8871ee1bf33bc1df6d54c5323e15dd288ef53
HickeryDickery <75381076+HickeryDickery@users.noreply.github.com>
2023-04-04 23:44:50 +0500

    Screen: Profile. Components: Header, Navbar

---
+++ luminous/components/Navbar.jsx
@@ -0,0 +1,168 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+
+const Navbar = ({ navigation, currentScreen }) => {
+  const handleEvent = async () => {
+    console.log("Navigating to Event Screen");
+    navigation.navigate("events");
+  };
+
+  const handleProfile = () => {
+    console.log("Navigating to Profile Screen");
+    navigation.navigate("profile");
+  };
+
+  const handleHome = () => {
+    console.log("Navigating to Home Screen");
+    navigation.navigate("homescreen");
+  };
+
+  return (
+    <View style={styles.navBar}>
+      <TouchableOpacity
+        stylestyle={
+          currentScreen === "map"
+            ? [styles.navButton, styles.selectedNavButton]
+            : styles.navButton
+        }
+        v
+      >
+        <Ionicons
+          name="map-outline"
+          size={24}
+          color={currentScreen === "map" ? "blue" : "#aaa"}
+        />
+        <Text
+          style={
+            currentScreen === "map"
+              ? [styles.navText, styles.selectedNavText]
+              : styles.navText
+          }
+        >
+          Map
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={
+          currentScreen === "events"
+            ? [styles.navButton, styles.selectedNavButton]
+            : styles.navButton
+        }
+        onPress={handleEvent}
+      >
+        <Ionicons
+          name="calendar-outline"
+          size={24}
+          color={currentScreen === "events" ? "blue" : "#aaa"}
+        />
+        <Text
+          style={
+            currentScreen === "events"
+              ? [styles.navText, styles.selectedNavText]
+              : styles.navText
+          }
+        >
+          Events
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={
+          currentScreen === "home"
+            ? [styles.navButton, styles.selectedNavButton]
+            : styles.navButton
+        }
+        onPress={handleHome}
+      >
+        <Ionicons
+          name="home-outline"
+          size={24}
+          color={currentScreen === "home" ? "blue" : "#aaa"}
+        />
+        <Text
+          style={
+            currentScreen === "home"
+              ? [styles.navText, styles.selectedNavText]
+              : styles.navText
+          }
+        >
+          Home
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={
+          currentScreen === "donate"
+            ? [styles.navButton, styles.selectedNavButton]
+            : styles.navButton
+        }
+      >
+        <Ionicons
+          name="heart-outline"
+          size={24}
+          color={currentScreen === "donate" ? "blue" : "#aaa"}
+        />
+        <Text
+          style={
+            currentScreen === "donate"
+              ? [styles.navText, styles.selectedNavText]
+              : styles.navText
+          }
+        >
+          Donate
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={
+          currentScreen === "profile"
+            ? [styles.navButton, styles.selectedNavButton]
+            : styles.navButton
+        }
+        onPress={handleProfile}
+      >
+        <Ionicons
+          name="person-outline"
+          size={24}
+          color={currentScreen === "profile" ? "blue" : "#aaa"}
+        />
+        <Text
+          style={
+            currentScreen === "profile"
+              ? [styles.navText, styles.selectedNavText]
+              : styles.navText
+          }
+        >
+          Profile
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  navBar: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    height: 60,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
+    backgroundColor: "#000",
+  },
+  navButton: {
+    alignItems: "center",
+    flex: 1,
+  },
+  navText: {
+    fontSize: 12,
+    color: "#aaa",
+  },
+  selectedNavButton: {
+    borderBottomColor: "blue",
+    borderBottomWidth: 2,
+  },
+  selectedNavText: {
+    color: "blue",
+  },
+});
+
+export default Navbar;
--- luminous/screens/Profile.jsx
+++ luminous/screens/Profile.jsx
@@ -1,28 +1,183 @@
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { Avatar } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { useState } from "react";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Profile({ navigation }) {
   const [avatar, setAvatar] = useState("");
 
+  const tempProfilePic = "../assets/profile.png";
+
   return (
-    <LinearGradient style={styles.container} colors={["#000000", "#0E2C4F"]}>
-      <Avatar.Image source={{ uri: avatar ? avatar : null }} size={100} />
-      <Text style={{ color: "white" }}>Profile</Text>
-    </LinearGradient>
+    <View style={{ flex: 1 }}>
+      <LinearGradient style={styles.container} colors={["#000000", "#0E2C4F"]}>
+        <Header name="Profile" />
+        <View
+          style={{
+            backgroundColor: "white",
+            width: "90%",
+            height: 2,
+            marginBottom: 20,
+          }}
+        ></View>
+        <Avatar.Image
+          source={require(tempProfilePic)}
+          size={150}
+          style={{ backgroundColor: "white" }}
+          resizeMode="cover"
+        />
+        <Text style={{ color: "white", fontSize: 30 }}>Name</Text>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 15,
+            marginBottom: 20,
+          }}
+        >
+          Username
+        </Text>
+        <View
+          style={{
+            backgroundColor: "white",
+            width: "90%",
+            height: 1,
+            // marginVertical: 20,
+          }}
+        ></View>
+
+        <ScrollView
+          style={{
+            // alignSelf: "flex-start",
+            flex: 1,
+            marginVertical: 10,
+            marginHorizontal: 40,
+            width: "85%",
+          }}
+        >
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="person"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>Change Username</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="lock"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>Change Password</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="notifications"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>Notifications</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="help"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>Help</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="info"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>About</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+
+          <TouchableOpacity style={styles.option}>
+            <MaterialIcons
+              name="power-settings-new"
+              size={24}
+              color="white"
+              style={{ paddingRight: 10 }}
+            />
+            <Text style={styles.optionsText}>Logout</Text>
+          </TouchableOpacity>
+          <View style={styles.border}></View>
+        </ScrollView>
+      </LinearGradient>
+      <Navbar navigation={navigation} currentScreen="profile" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "black",
     flex: 1,
     flexDirection: "column",
     justifyContent: "flex-start",
-    // height: "100%",
     alignItems: "center",
-    paddingTop: 80,
+    paddingTop: 50,
+  },
+
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+    marginTop: -30,
+    marginBottom: -15,
+    marginRight: -15,
+  },
+
+  header: {
+    display: "flex",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+
+  option: {
+    display: "flex",
+    flexDirection: "row",
+    paddingVertical: 10,
+  },
+
+  optionsText: {
+    color: "white",
+    fontSize: 18,
+  },
+
+  border: {
+    height: 1,
+    width: "100%",
+    backgroundColor: "white",
+    opacity: 0.5,
   },
 });
