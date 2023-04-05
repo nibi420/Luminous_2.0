c1867159d75ff0f4c3d32a919054f25eaa655395
Sarfraz30 <118740031+Sarfraz30@users.noreply.github.com>
2023-04-05 20:39:31 +0500

    donations backend made

---
+++ server/controllers/Donations.js
@@ -0,0 +1,24 @@
+import { Donation } from "../models/donation.js";
+import { User } from "../models/users.js";
+import { sendMail } from "../utils/sendMail.js";
+import { sendToken } from "../utils/sendToken.js";
+
+
+
+export const getDonationsData = async (req,res) => {
+   
+       const user = await Donation.find({  })
+       return res.send(user)
+       console.log(user)
+   
+  };
+
+export const pushDonationsData = async (req,res) => {
+    console.log(req.body)
+   
+    let don_case = await Donation.create(req.body);
+
+    return res.send("done")
+    console.log(user)
+
+};
