516c728369b2d92629603bd0d7303a72559aec2a
azmeer36 <92108835+azmeer36@users.noreply.github.com>
2023-04-16 09:15:52 +0500

    ignore

--- luminous/constant.js
+++ luminous/constant.js
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 // export const IP = "luminous.heroku.app.com"
 
 export const IP = "http://10.130.144.150:8000";
+=======
+export const IP = "https://luminous.herokuapp.com"
+// export const IP = "https://10.130.142.133:8000";
+>>>>>>> Stashed changes
